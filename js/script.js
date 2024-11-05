28a9e103f104f3f7a1622d3a7a2e5ed296a227ac
Guanjino <jjpereiranaranjo@gmail.com>
2024-11-05 22:01:04 +0100

    ejercicio_completado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
  // Aquí tu código
+
+const addBtn = document.getElementById('agregar');
+const lista = document.getElementById('lista');
+
+addBtn.addEventListener('click', function() {
+    const elemento = prompt('Añadir elemento a la lista');
+    if (elemento) {
+        const nuevoElemento = document.createElement('li');
+        nuevoElemento.textContent = elemento;
+        lista.appendChild(nuevoElemento);
+    } else {
+        alert('No se ha añadido ningún elemento a la lista') 
+    }
+})
+
+
